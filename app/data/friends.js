9295587725ccf89516c78e3c629284b177fc21ee
Nicholas Mahase <nicholasmahase@gmail.com>
2019-03-25 00:50:37 -0400

    updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,8 +6,8 @@
 
 var friends = [
   {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    name: "Richard",
+    photo: "",
     scores: [
       "5",
       "1",
@@ -22,8 +22,8 @@ var friends = [
     ]
   },
   {
-    name: "Jacob Deming",
-    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    name: "Khrish",
+    photo: "",
     scores: [
       "4",
       "2",
@@ -38,8 +38,8 @@ var friends = [
     ]
   },
   {
-    name: "Jeremiah Scanlon",
-    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    name: "Mike",
+    photo: "",
     scores: [
       "5",
       "2",
@@ -54,8 +54,8 @@ var friends = [
     ]
   },
   {
-    name: "Louis T. Delia",
-    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    name: "Angelo",
+    photo: "",
     scores: [
       "3",
       "3",
@@ -70,8 +70,8 @@ var friends = [
     ]
   },
   {
-    name: "Lou Ritter",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    name: "Semir",
+    photo: "",
     scores: [
       "4",
       "3",
@@ -86,8 +86,8 @@ var friends = [
     ]
   },
   {
-    name: "Jordan Biason",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    name: "Nick",
+    photo: "",
     scores: [
       "4",
       "4",
